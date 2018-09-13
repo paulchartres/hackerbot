a9821115fbdd54a0dec0702a60b7a370d6808cfb
paulchartres <paul.dielen@orange.fr>
2018-09-13 08:46:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const prefix = '$';
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -9,6 +11,9 @@ client.on('message', message => {
     if (message.content === 'zbeub') {
     	message.reply('zbeub');
   	}
+    if (message.startsWith(prefix + "despacito")) {
+        message.channel.send("Ta gueule.");
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
