7ddfeef3558f3cd81614f0985eaeaedbcdaed9dc
paulchartres <paul.dielen@orange.fr>
2018-09-13 15:09:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,18 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Commando = require('discord.js-commando');
+const bot = new Commando.Client();
 
 const prefix = '$';
 
-client.on('ready', () => {
+bot.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
+bot.on('message', message => {
     if (message.content === '$despacito') {
     	message.reply('ta gueule.');
   	}
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN);
 
