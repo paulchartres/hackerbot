2f0f0bef23689b1d87a7d0bb617f8ec465e31878
paulchartres <paul.dielen@orange.fr>
2018-09-13 15:18:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,12 +9,6 @@ bot.on('ready', () => {
     console.log('I am ready!');
 });
 
-bot.on('message', message => {
-    if (message.content === '$despacito') {
-    	message.reply('ta gueule.');
-  	}
-});
-
 // THIS  MUST  BE  THIS  WAY
 bot.login(process.env.BOT_TOKEN);
 
