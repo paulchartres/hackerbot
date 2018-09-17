f29ea34f46356030aa1b49c2ac2e2fc57e2b9013
paulchartres <paul.dielen@orange.fr>
2018-09-17 10:29:40 +0200

    Finished timer function

--- commands/utilitaires/timer.js
+++ commands/utilitaires/timer.js
@@ -32,10 +32,20 @@ class TimerCommand extends commando.Command
         var p_12h_h = 11-h_current
         var p_12h_m = ((12-h_current)*60)-m_current-(p_12h_h*60)
 
+        var p_15h_h = 14-h_current
+        var p_15h_m = ((15-h_current)*60)-m_current-(p_15h_h*60)
+
+        var p_17h_h = 16-h_current
+        var p_17h_m = ((17-h_current)*60)-m_current-(p_17h_h*60)-15
+
         if (h_current < 10) {
             message.channel.send("Il reste " + p_10h_h + " heures et " + p_10h_m + " minutes avant la pause de 10h.")
         } else if (h_current >= 10 && h_current < 12) {
             message.channel.send("Il reste " + p_12h_h + " heures et " + p_12h_m + " minutes avant la pause de midi.")
+        } else if (h_current >= 12 && h_current < 15) {
+            message.channel.send("Il reste " + p_15h_h + " heures et " + p_15h_m + " minutes avant la pause de 15h.")
+        } else if (h_current >= 15 && h_current < 17) {
+            message.channel.send("Il reste " + p_17h_h + " heures et " + p_17h_m + " minutes avant la fin des cours.")
         }
     }
 }
