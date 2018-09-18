23e75ba394f53f061b0af762e9166f5a6cdd146d
paulchartres <paul.dielen@orange.fr>
2018-09-18 12:00:18 +0200

    Updated antispam

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
 const Commando = require('discord.js-commando');
+const Discord = require('discord.js');
 const bot = new Commando.Client();
+
+var anti_spam = require("discord-anti-spam");
 
 bot.registry.registerGroup('funny', 'Inutile');
 bot.registry.registerGroup('reactions', 'Réactions');
@@ -12,6 +15,17 @@ bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + '/commands');
 
 global.servers = {};
+
+antispam(bot, {
+  warnBuffer: 3, //Maximum amount of messages allowed to send in the interval time before getting warned.
+  maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned.
+  interval: 1000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
+  warningMessage: "stop spamming or I'll whack your head off.", // Warning message send to the user indicating they are going to fast.
+  banMessage: "has been banned for spamming, anyone else?", // Ban message, always tags the banned user in front of it.
+  maxDuplicatesWarning: 7,// Maximum amount of duplicate messages a user can send in a timespan before getting warned
+  maxDuplicatesBan: 10, // Maximum amount of duplicate messages a user can send in a timespan before getting banned
+  deleteMessagesAfterBanForPastDays: 7 // Delete the spammed messages after banning for the past x days.
+});
 
 bot.on('ready', function(){
     bot.user.setActivity('hacker le FBI').catch(console.error)
