b262441b6a94b82cc53e505af1d8efbfa7d068a2
paulchartres <paul.dielen@orange.fr>
2018-09-27 08:55:47 +0200

    Added $hb command

---
+++ commands/funny/hb.js
@@ -0,0 +1,26 @@
+const commando = require('discord.js-commando');
+
+class HBCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'hb',
+            group: 'funny',
+            memberName: 'hb',
+            description: 'Si tu veux souhaiter un anniversaire à quelqu\'un, utilises cette commande avec le tag de l\'utilisateur!'
+        });
+    }
+
+    async run(message, args)
+    {
+        switch (args) {
+            case "<@188295851813109760>":
+                message.channel.send("Test");
+            default:
+                message.channel.send("Test2");
+        }
+    }
+}
+
+module.exports = HBCommand;
