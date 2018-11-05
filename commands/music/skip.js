86b490ccf0b67d62704d98f40efe3788657dff82
p.dielenschneider <p.dielenschneider@intech-sud.fr>
2018-11-05 10:59:38 +0100

    Changed stuff

---
+++ commands/music/skip.js
@@ -0,0 +1,51 @@
+const commando = require('discord.js-commando');
+const YTDL = require('ytdl-core');
+
+function Play(connection, message)
+{
+    var server = servers[message.guild.id];
+    server.dipatcher.destroy();
+    server.dipatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}))
+    server.queue.shift();
+    server.dipatcher.on('end', function(){
+        if (server.queue[0])
+        {
+            Play(connection,message);
+        }
+        else
+        {
+            connection.disconnect();
+        }
+    })
+}
+
+class SkipCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'skip',
+            group: 'music',
+            memberName: 'skip',
+            description: 'Quand un morceau est juste mauvais, tu peux le passer avec cette commande.'
+        });
+    }
+
+    async run(message, args)
+    {
+        if (message.guild.voiceConnection)
+        {
+            var server = servers[message.guild.id];
+            message.channel.send('J\'ai passé le morceau.');
+            message.member.voiceChannel.join()
+                .then(connection =>{
+                    var server = servers[message.guild.id];
+                    Play(connection, message);
+                })
+        } else {
+            message.channel.send('Je dois être dans un salon vocal pour pouvoir passer le morceau.');
+        }
+    }
+}
+
+module.exports = SkipCommand;
